e6f0f8bcde78453c456502a653c41ad51be6066d
shauryas2021 <85066104+shauryas2021@users.noreply.github.com>
2022-04-18 17:13:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+song1 = "";
+song2 = "";
+song1_status = "";
+song2_status = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+
+var scoreLeftWrist = 0;
+function preload(){
+song1 = loadSound("music.mp3");
+song2 = loadSound("spider_man.mp3");
+}
+function setup()
+{
+   canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function play()
+{
+    song1.stop();
+    song2.play();
+    document.getElementById("name").innerHTML = "Song Name = Spider Man : Homecoming theme"
+    song2.setVolume(1);
+    song2.rate(1);
+    song1.setVolume(1);
+    song1.rate(1);
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+    if (scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+     song2.stop();
+
+     if (song1_status == false){
+         song1.play();
+         document.getElementById("name").innerHTML = "Music Name = Harry Potter Theme"
+     }
+    }
+    if (scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(song2_status == false){
+            song2.play();
+            document.getElementById("name").innerHTML = "Music Name = Spider Man Homecoming Theme"
+        }
+    }
+    
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+}
+function gotPoses(results){
+if (results.length > 0){
+    console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("LeftWristX = " + leftWristX + " LeftWristY = " + leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+   rightWristY = results[0].pose.rightWrist.y;
+    console.log("RightWristX = " + rightWristX + " RighttWristY = " + rightWristY);
+}
+}
